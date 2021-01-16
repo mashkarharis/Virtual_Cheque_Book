35ce976c6088543c94af05cbc01b754071b13d8b
mashkarharis <mashkarharis@gmail.com>
2021-01-16 11:41:32 +0530

    Deleted

--- routes/StaffRouter.js
+++
@@ -1,9 +0,0 @@
-// Express Initialize
-const express=require('express')
-const router=express.Router()
-
-router.get('/', (req,res)=> {
-    res.send("You Visited /staff/");
-});
-
-module.exports=router;
