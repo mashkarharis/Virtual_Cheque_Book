7f2bc36a91ec3ff91b25951358777a31a885f207
nipdep <nipun1deelaka@gmail.com>
2021-01-20 20:04:50 +0530

    Edit: debugged procedures

--- document/test_queries.sql
+++ document/test_queries.sql
@@ -1,1 +1,15 @@
 call user_reg('username', 'password', 1234567891, 'full_name', 'name_with_init', '2001-01-23', '2020-01-11', '766855983V', 'Male', '123/1', 'street', 'city', 10320000, 123456, 0718908140, 0174397258);
+
+call user_reg('usernam1', 'passwors', 1234567892, 'full_nam1', 'name_with_ini1', '2001-02-23', '2020-02-11', '766855383V', 'Male', '123/2', 'streee', 'ciiy', 11320000, 133456, 0728908140, 0274397258);
+
+call user_reg('usernam34', 'passwors', 1234367892, 'full_nam1', 'name_with_ini1', '2001-02-23', '2020-02-11', '76615383V', 'Male', '123/2', 'streee', 'ciiy', 11320000, 133456, 0728908140, 0274397258);
+
+call approve_acc(50);
+
+call update_cheque_status(7, "EVALUATING");
+
+call set_pin_forget(39);
+
+call update_pin(39, 654321);
+
+call update_noti_to_read(3);
